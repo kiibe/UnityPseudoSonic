4e69bfc1af3afa6072fa3b32cdbbc043d8dac24d
kiibe <jac274@gmail.com>
2016-04-25 16:53:18 +0200

    origin commit

---
+++ the way of gobling/Assets/Scrips/ControladorPersonaje.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorPersonaje : MonoBehaviour {
+	//variables salto
+	public float fuerzaSalto = 400f;
+	public Rigidbody2D rb;
+	private bool enSuelo = true;
+	public Transform comprobadorSuelo;
+	float comprobadorRadio = 0.1f;
+	public LayerMask mascaraSuelo;
+	private Animator animator;
+
+	//variables correr
+	private bool corriendo = false;
+	public float velocidad = 5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Get the instance here and stores it as a class member.
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Awake()
+	{
+		animator = GetComponent<Animator> ();
+	}
+
+	void FixedUpdate()
+	{
+		if (corriendo) 
+		{
+			rb.velocity = new Vector2 (velocidad, rb.velocity.y);
+		}
+		animator.SetFloat ("VelX", rb.velocity.x);
+		enSuelo = Physics2D.OverlapCircle (comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
+		animator.SetBool ("isGrounded", enSuelo);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			if (corriendo) {
+				//hacer que salte si puede saltar
+				if (enSuelo) 
+				{
+					rb.AddForce (new Vector2 (0, fuerzaSalto));
+				}
+			} 
+			else 
+			{
+				corriendo = true;
+			}
+		
+		}
+	}
+}
---
+++ the way of gobling/Assets/Scrips/SeguirPersonaje.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeguirPersonaje : MonoBehaviour {
+
+	public Transform personaje;
+	public float separacion = 6f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = new Vector3 (personaje.position.x + separacion,personaje.position.y,personaje.position.z -25f);
+	}
+}
