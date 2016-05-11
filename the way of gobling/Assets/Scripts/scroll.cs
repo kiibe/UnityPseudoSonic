32cab3e77776b772c4a7f2bfee9f8d1fea0f4952
miquelcms <miquel.cms@gmail.com>
2016-05-11 18:51:56 +0200

    background

---
+++ the way of gobling/Assets/Scripts/scroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+    public float velocidad = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float camera = Camera.main.gameObject.transform.position.x;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(camera * velocidad, 0);
+
+	}
+}
